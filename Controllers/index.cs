5a435f8c49c1d71dd91c136d052659e59f1cce71
eyu-dev <169823478+eyu-dev@users.noreply.github.com>
2024-05-28 19:08:16 +0300

    Initial commit
    Signed-off-by: eyu-dev <169823478+eyu-dev@users.noreply.github.com>

---
+++ Controllers/index.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+using Demo_Project.Models;
+
+namespace Demo_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class index : ControllerBase
+    {
+
+        //[HttpPost]
+        //public async Task<JsonResult> PostAsync([FromBody] Request request)
+        //{
+        //    var biz = request;
+        //    return JsonResult(new { title = request.Title, amount = biz.Amount });
+        //}
+    }
+}
